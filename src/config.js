1e1ba6f385b888e23be165dbf0992e35993cd8f8
Parker Brody <parker@unknownba071d0a93f3.attlocal.net>
2025-11-02 01:20:51 -0500

    config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
-// src/config.js
-const API_BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:8000"
-    : "https://parkerlbrody-lemd.hf.space";
+const API_BASE_URL = "https://parkerlbrody-lemd.hf.space";
 
 export default API_BASE_URL;
